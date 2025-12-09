c9e3a2e367675d39ed086bbe4cfac5de2cf7dbe8
morad <KARKARMORAD29@GMAIL.COM>
2025-12-09 16:49:10 +0100

    ho un problema nell'app.jsx e con gli immagini

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,21 @@
 function Header() {
     return (
         <header>
-            <img src="./img/dc-logo.png" alt="" />
+            <img src="/dc-logo.png" alt="" />
+            <nav>
+                <ul>
+                    <li><a href="#"></a>CHARACTERS</li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#"></a>MOVIES</li>
+                    <li><a href="#"></a>TV</li>
+                    <li><a href="#"></a>GAMES</li>
+                    <li><a href="#"></a>COLLECTIBLES</li>
+                    <li><a href="#"></a>VIDEOS</li>
+                    <li><a href="#"></a>FANS</li>
+                    <li><a href="#"></a>NEWS</li>
+                    <li><a href="#"></a>SHOP</li>
+                </ul>
+            </nav>
         </header>
 
     );
