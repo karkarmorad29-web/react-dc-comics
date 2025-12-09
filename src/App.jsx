ae534e7b48663e8a0dd489563ca7001ae11f0bcf
morad <KARKARMORAD29@GMAIL.COM>
2025-12-09 11:08:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+function App() {
+
+
+  return (
+    <div id="app">
+      <header />
+      <h1>ciao</h1>
+
+    </div >
+
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header() {
+    return (
+        <header>
+            <img src="./img/dc-logo.png" alt="" />
+        </header>
+
+    );
+}
+
+export default Header;
