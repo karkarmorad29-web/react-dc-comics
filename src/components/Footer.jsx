0a12681e54848e5806537efda92c9e2417e5bbeb
morad <KARKARMORAD29@GMAIL.COM>
2025-12-10 21:07:45 +0100

    nuovo Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    return (
+        <footer>
+            <div className="Footer-container">
+                <div className="colonna">
+                    <img src="/img/buy-comics-digital-comics.png" alt="" />
+                    <img src="/img/buy-comics-merchandise.png" alt="" />
+                    <img src="/img/buy-comics-subscriptions.png" alt="" />
+                    <img src="/img/buy-comics-shop-locator.png" alt="" />
+                    <img src="/img/buy-dc-power-visa.svg" alt="" />
+                </div>
+
+                <div className="colonna">
+                    <h1>ciao</h1>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
