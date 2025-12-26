f8f1d949bb027b9270f620a6b87145b0863e29e5
morad <KARKARMORAD29@GMAIL.COM>
2025-12-26 16:11:08 +0100

    solo con footerSections la pagina rimane vuota!!??

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,97 +1,6 @@
+import footerSections from "../data/footerItems.js";
+
 function Footer() {
-
-    const DcComicslists = [
-        { id: 1, label: "Characters", href: "#" },
-        { id: 2, label: "Comics", href: "#" },
-        { id: 3, label: "Movies", href: "#" },
-        { id: 4, label: "TV", href: "#" },
-        { id: 5, label: "Games", href: "#" },
-        { id: 6, label: "Videos", href: "#" },
-        { id: 7, label: "News", href: "#" }
-    ];
-
-
-    const Shoplists = [
-        { id: 8, label: "Shop DC", href: "#" },
-        { id: 9, label: "Shop DC Collectibles", href: "#" }
-    ];
-
-
-    const DClists = [
-        { id: 10, label: "Terms Of Use", href: "#" },
-        { id: 11, label: "Privacy policy (New)", href: "#" },
-        { id: 12, label: "Ad Choices", href: "#" },
-        { id: 13, label: "Advertising", href: "#" },
-        { id: 14, label: "Jobs", href: "#" },
-        { id: 15, label: "Subscriptions", href: "#" },
-        { id: 16, label: "Talent Workshops", href: "#" },
-        { id: 17, label: "CPSC Certificates", href: "#" },
-        { id: 18, label: "Ratings", href: "#" },
-        { id: 19, label: "Shop Help", href: "#" },
-        { id: 20, label: "Contact Us", href: "#" }
-    ];
-
-
-    const Siteslists = [
-        { id: 21, label: "DC", href: "#" },
-        { id: 22, label: "MAD Magazine", href: "#" },
-        { id: 23, label: "DC Kids", href: "#" },
-        { id: 24, label: "DC Universe", href: "#" },
-        { id: 25, label: "DC Power Visa", href: "#" }
-    ];
-
-
-    const footerSections = [
-        {
-            title: "DC COMICS", items: [
-                { id: 1, label: "Characters", href: "#" },
-                { id: 2, label: "Comics", href: "#" },
-                { id: 3, label: "Movies", href: "#" },
-                { id: 4, label: "TV", href: "#" },
-                { id: 5, label: "Games", href: "#" },
-                { id: 6, label: "Videos", href: "#" },
-                { id: 7, label: "News", href: "#" }
-            ]
-        },
-
-
-        {
-            title: "SHOP", items: [
-                { id: 8, label: "Shop DC", href: "#" },
-                { id: 9, label: "Shop DC Collectibles", href: "#" }
-            ]
-        },
-
-
-        {
-            title: "DC", items: [
-                { id: 10, label: "Terms Of Use", href: "#" },
-                { id: 11, label: "Privacy policy (New)", href: "#" },
-                { id: 12, label: "Ad Choices", href: "#" },
-                { id: 13, label: "Advertising", href: "#" },
-                { id: 14, label: "Jobs", href: "#" },
-                { id: 15, label: "Subscriptions", href: "#" },
-                { id: 16, label: "Talent Workshops", href: "#" },
-                { id: 17, label: "CPSC Certificates", href: "#" },
-                { id: 18, label: "Ratings", href: "#" },
-                { id: 19, label: "Shop Help", href: "#" },
-                { id: 20, label: "Contact Us", href: "#" }
-            ]
-        },
-
-        {
-            title: "SITES", items: [
-                { id: 21, label: "DC", href: "#" },
-                { id: 22, label: "MAD Magazine", href: "#" },
-                { id: 23, label: "DC Kids", href: "#" },
-                { id: 24, label: "DC Universe", href: "#" },
-                { id: 25, label: "DC Power Visa", href: "#" }
-            ]
-        }
-    ];
-
-
-
 
     return (
         <footer>
@@ -114,8 +23,8 @@ function Footer() {
                 {footerSections.map(sezione => {
                     <div>
                         <h4>{sezione.title}</h4>
-                        <ul className="first-ul">
-                            {sezione.items.map(item => {
+                        <ul>{
+                            sezione.items.map(item => {
                                 return <li><a href={item.href} key={item.id}>{item.label}</a></li>
                             })}
                         </ul>
